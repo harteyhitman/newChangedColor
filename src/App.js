36634682e7fad72eecec0257dfa922f7a2442796
harteyhitman <oyegoke.a18@gmail.com>
2023-02-07 08:23:36 +0100

    mobileviewcolors

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react"
+import Tweet from "./Tweet"
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [isRed, setRed] = useState(false)
+  const [count, setCount] = useState(0)
+
+  const Increment = () => {
+    setCount(count + 1)
+    setRed(!isRed);
+  }
+
+  // const [users, setUser] = useState([
+  //   {name: "Ed", message: "Hello dear"},
+  //   {name: "John", message: "Whats going on"},
+  //   {name: "Ed", message: "Im coming Home"}
+  // ])
+
+  return(
+    <div className="app">
+      <h1 className={isRed ? "red": "" }>Change my color</ h1>
+         <button onClick={Increment}>Increment</button>
+         <h1>{count}</h1>
+    <Tweet name="Oyegoke Azeez" desc="I keep learning react" likes="no: 125"/>
     </div>
-  );
+  )
+  
 }
 
-export default App;
+export default App
